8ef0e9e6eaf1a13c2b744de6f3fd7bd18443115d
Ayush Mehrotra <48199435+Ayush900@users.noreply.github.com>
2025-01-13 17:39:55 +0530

    Update build.sh

--- go-release-executables/build.sh
+++ go-release-executables/build.sh
@@ -20,7 +20,6 @@ sudo rmdir $PROJECT_ROOT
 # without above, following symlink creation fails (?)
 sudo ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
 cd $PROJECT_ROOT/${SUBDIR}
-pwd
 go get -v ./...
 
 EXT=''
