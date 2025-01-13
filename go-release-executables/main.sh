5bfa69fba1b072bccd30181132e7aa38195b6e05
Ayush Mehrotra <48199435+Ayush900@users.noreply.github.com>
2025-01-13 13:17:10 +0530

    Update main.sh

--- go-release-executables/main.sh
+++ go-release-executables/main.sh
@@ -4,29 +4,8 @@ set -x
 
 #!/bin/bash -eux
 
-createNameForOS() {
-    OS="$1"
-    EXECUTABLE_FILES="$2"
-    RELEASE_TAG_NAME="$3"
-    TMP_ARCHIVE="$4"
-    GITHUB_TOKEN="$5"
-    RELEASE_UPLOAD_URL="$6"
-
-    retn=(${OS}_386 ${OS}_amd64)
-    for item in "${retn[@]}"; do
-        NAME="${NAME:-${EXECUTABLE_FILES}_${RELEASE_TAG_NAME}}_${item}"
-        curl \
-          --tlsv1.2 \
-          -X POST \
-          --data-binary @${TMP_ARCHIVE} \
-          -H 'Content-Type: application/octet-stream' \
-          -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-          "${RELEASE_UPLOAD_URL}?name=${NAME}.${TMP_ARCHIVE/tmp./}"
-            done
-}
-
 GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.14.linux-amd64.tar.gz"
-wget --no-check-certificate --progress=dot:mega ${GO_LINUX_PACKAGE_URL} -O go-linux.tar.gz
+wget --no-check-certificate --progress=dot:mega ${GO_LINUX_PACKAGE_URL} -O go-linux.tar.gz 
 tar -zxf go-linux.tar.gz
 mv go /usr/local/
 mkdir -p /go/bin /go/src /go/pkg
@@ -48,21 +27,14 @@ PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
 TMP_ARCHIVE=tmp.tgz
 CKSUM_FILE=md5sum.txt
 md5sum ${PROJECT_ROOT}/${SUBDIR}/${EXECUTABLE_FILES} | cut -d ' ' -f 1 > ${CKSUM_FILE}
-tar cvfz ${TMP_ARCHIVE} ${CKSUM_FILE} --directory ${PROJECT_ROOT}/${SUBDIR} ${EXECUTABLE_FILES}
+tar cvfz ${TMP_ARCHIVE} ${CKSUM_FILE} --directory ${PROJECT_ROOT}/${SUBDIR} ${EXECUTABLE_FILES} 
 
-os_list=(linux windows darwin)
+NAME="${NAME:-${EXECUTABLE_FILES}_${RELEASE_TAG_NAME}}_${GOOS}_${GOARCH}"
 
-for i in "${os_list[@]}"; do
-    createNameForOS $i $EXECUTABLE_FILES $RELEASE_TAG_NAME $TMP_ARCHIVE $GITHUB_TOKEN $RELEASE_UPLOAD_URL
-done
-
-
-# NAME="${NAME:-${EXECUTABLE_FILES}_${RELEASE_TAG_NAME}}_${GOOS}_${GOARCH}"
-
-# curl \
-#   --tlsv1.2 \
-#   -X POST \
-#   --data-binary @${TMP_ARCHIVE} \
-#   -H 'Content-Type: application/octet-stream' \
-#   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-#   "${RELEASE_UPLOAD_URL}?name=${NAME}.${TMP_ARCHIVE/tmp./}"
+curl \
+  --tlsv1.2 \
+  -X POST \
+  --data-binary @${TMP_ARCHIVE} \
+  -H 'Content-Type: application/octet-stream' \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "${RELEASE_UPLOAD_URL}?name=${NAME}.${TMP_ARCHIVE/tmp./}"
